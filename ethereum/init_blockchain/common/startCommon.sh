9a2230c0ca86e28bf16dc4045512218bef9cf12e
Roman Fedorov <Roman2x@yandex.ru>
2018-05-24 19:42:40 +0300

    Migrate to latest ethereum version, fix genesis file

--- ethereum/init_blockchain/common/startCommon.sh
+++ ethereum/init_blockchain/common/startCommon.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-export GETH_COMMON_PROPERTIES="--datadir=/home/ubuntu/eth-root/datadir --targetgaslimit 100 --gasprice 1000 --networkid 497 --rpc --rpcport 8101 --rpccorsdomain \"*\" --rpcaddr 0.0.0.0 --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3 --port 30303";
-export GETH="/home/ubuntu/gopath/go-ethereum/build/bin/geth ${GETH_COMMON_PROPERTIES}";
+export GETH_COMMON_PROPERTIES="--datadir=/home/ubuntu/eth-root/datadir --rpc --rpcport 8101 --rpccorsdomain \"*\" --rpcaddr 0.0.0.0 --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3 --port 30303";
+export GETH="geth ${GETH_COMMON_PROPERTIES}";
 echo $GETH;
 
-$GETH init ~/common_init_files/root_init_result/genesis.json;
-cp ~/common_init_files/root_init_result/keystore/* ~/eth-root/datadir/keystore
+$GETH init /home/ubuntu/common_init_files/root_init_result/genesis.json;
+cp /home/ubuntu/common_init_files/root_init_result/keystore/* /home/ubuntu/eth-root/datadir/keystore;
 
 mkdir /home/ubuntu/.ethash;
 $GETH makedag 0 /home/ubuntu/.ethash >makedag1 2>makedag2;
 
-nohup $GETH --mine --minerthreads=1 --etherbase=$(shuf -n 1 ~/common_init_files/root_init_result/accounts) --bootnodes $(cat ~/common_init_files/root_init_result//enode) >/dev/null 2>ethereum.log &
+nohup $GETH --mine --minerthreads=1 --etherbase=$(shuf -n 1 ~/common_init_files/root_init_result/accounts) --bootnodes="$(cat ~/common_init_files/root_init_result/enode)" >/dev/null 2>ethereum.log &
--- ethereum/init_blockchain/initEnv.sh
+++ ethereum/init_blockchain/initEnv.sh
@@ -1,24 +1,12 @@
 #!/usr/bin/env bash
 
 sudo apt-get update;
-#sudo apt-get -y upgrade;
 sudo apt-get -y install default-jre
-wget https://storage.googleapis.com/golang/go1.7.4.linux-amd64.tar.gz;
-sudo tar -xvf go1.7.4.linux-amd64.tar.gz;
-sudo mv go /usr/local;
-export GOROOT=/usr/local/go;
-export GOPATH=$HOME/gopath;
-export PATH=$GOPATH/bin:$GOROOT/bin:$PATH;
 
-
-mkdir gopath;
-cd gopath;
-git clone -b release/1.6 https://github.com/ethereum/go-ethereum;
-sudo apt-get install -y build-essential golang;
-
-
-cd go-ethereum;
-make all;
+sudo apt-get install software-properties-common
+sudo add-apt-repository -y ppa:ethereum/ethereum
+sudo apt-get update
+sudo apt-get install -y ethereum
 
 cd ~;
 mkdir eth-root;
