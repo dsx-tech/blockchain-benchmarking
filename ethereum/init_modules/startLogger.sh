723f1fe991c5c8dfd7b7ca2a2a10274c5bc713d4
Roman Fedorov <Roman2x@yandex.ru>
2018-06-02 21:19:35 +0300

    Define Xms Xmx for jar applications (Network manager, logger, resource monitor)

--- ethereum/init_modules/startLogger.sh
+++ ethereum/init_modules/startLogger.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-nohup java -jar blockchain-logger.jar ${LOG_PARAMS} >/dev/null 2>blockchain-logger-stdout.log &
+nohup java -Xms64m -Xmx128m -jar blockchain-logger.jar ${LOG_PARAMS} >/dev/null 2>blockchain-logger-stdout.log &
--- ethereum/init_modules/startResourceMonitor.sh
+++ ethereum/init_modules/startResourceMonitor.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-nohup java -jar resource-monitor.jar >/dev/null 2>resource-monitor-stdout.log &
+nohup java -Xms64m -Xmx128m -jar resource-monitor.jar >/dev/null 2>resource-monitor-stdout.log &
